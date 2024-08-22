dfd09bbdc7fc2a910a2a67a662d65083049900eb
carolina.freitas <carolsoaresfreitas@hotmail.com>
2024-08-22 14:51:50 -0300

    initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,44 @@
-import { HelloWave } from "@/components/HelloWave";
-import { Text, View } from "react-native";
+// pages/Index.tsx
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Projeto de Frameworks para Desenv. Web I</Text>
-      <HelloWave />
+    <View style={styles.container}>
+      <View style={styles.gradientContainer}>
+        <Text style={styles.gradientText}>Projeto de Frameworks para Desenv. Web I</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2f2f2',
+  },
+  gradientContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: 100,
+  },
+  gradientText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    fontFamily: 'Poppins_700Bold', // Use a fonte carregada
+  },
+});
