61be3e191f077cad26dbaed26f8c703f5f0567fb
carolina.freitas <carolsoaresfreitas@hotmail.com>
2024-08-22 15:01:47 -0300

    initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,3 @@
-// pages/Index.tsx
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
@@ -16,8 +15,8 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.gradientContainer}>
-        <Text style={styles.gradientText}>Projeto de Frameworks para Desenv. Web I</Text>
+      <View >
+        <Text style={styles.text}>Projeto de Frameworks para Desenv. Web I</Text>
       </View>
     </View>
   );
@@ -30,15 +29,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#f2f2f2',
   },
-  gradientContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    height: 100,
-  },
-  gradientText: {
+  text: {
     fontSize: 24,
     fontWeight: 'bold',
-    fontFamily: 'Poppins_700Bold', // Use a fonte carregada
+    fontFamily: 'Poppins_700Bold',
+    color: 'purple',
+    shadowColor: 'black',
+    'textShadowOffset': { width: 2, height: 2 },
   },
 });
