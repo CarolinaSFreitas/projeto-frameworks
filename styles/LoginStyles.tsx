cf4425eac41c47b6a451504cd5bab254e1600609
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2024-09-04 23:00:43 +0000

    add

---
+++ styles/LoginStyles.tsx
@@ -0,0 +1,12 @@
+///acho que essa tela morrerá e ficará no index.tsx...
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+    
+    
+    
+    }
+});
+
+export default styles;
