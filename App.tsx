5903aececfdaa6ee0c9bbf6f8627fb58156c8fbd
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2024-09-11 22:46:25 +0000

    edit

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import * as Font from 'expo-font';
 import Login from './app/(screens)/Login';
 import Signup from './app/(screens)/Signup';
-import Navigation from './app/(navigation)/Navigation'; // Atualize o caminho conforme necessário
+import Navigation from './app/(navigation)/Navigation'; 
 
 const Stack = createStackNavigator();
 
