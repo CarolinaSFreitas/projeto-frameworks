63d4a412415361deba078b021b1b394f781f8747
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2024-09-19 18:44:36 +0000

    Border edit

--- styles/HomeStyles.tsx
+++ styles/HomeStyles.tsx
@@ -96,7 +96,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         padding: 20,
-        borderRadius: 20,
+        borderRadius: 12,
         overflow: 'hidden',
     },
     textContainer: {
